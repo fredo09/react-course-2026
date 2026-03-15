dc7e3b7a7fa09128c779fea90249a002d68814d9
fredo09 <fred_15@outlook.com>
2026-03-15 15:57:24 -0600

    feat: actualizar pruebas de FirstStepApp y ajustar lógica de división en math.helper

--- src/helpers/math.helper.ts
+++ src/helpers/math.helper.ts
@@ -13,7 +13,8 @@ export const multiply = (a: number, b: number): number => {
 
 export const divide = (a: number, b: number): number => {
 	if (b === 0) {
-		throw new Error("Cannot divide by zero");
+		// throw new Error("Cannot divide by zero");
+		return 0;
 	}
 	return a / b;
 }
--- src/test/MyStepApp.spec.tsx
+++ src/test/MyStepApp.spec.tsx
@@ -1,11 +1,27 @@
-import { render } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
 import { FirstStepApp } from '../FirstStepApp';
+
+/**
+ * Simulacion de una dependencia para este caso es un componente que se llama ItemCounter, lo que hacemos es simularlo para que no afecte a nuestras pruebas, esto es util cuando el componente tiene una logica compleja o consume recursos externos, de esta manera podemos aislar el componente que queremos probar y evitar que las pruebas fallen por problemas en la dependencia.
+ */
+vi.mock('./../shopping-cart/ItemCounter.tsx',  () => ({
+	ItemCounter: () => <div data-testId="item-counter"/>,
+}));
 
 describe('FirstStepApp', () => {
 	test('should render the component', () => {
 		const { container } = render(<FirstStepApp />);
 
+		// ayuda a validar que el componente no cambie, si cambia, la prueba falla 
 		expect(container).toMatchSnapshot();
+	});
+
+	test('Should render rthe correct number of ItemContainer components', () => {
+		render(<FirstStepApp/>);
+		screen.debug();
+
+		const itemCounterElements = screen.getAllByTestId('item-counter');
+		expect(itemCounterElements.length).toBe(3);
 	});
 });
